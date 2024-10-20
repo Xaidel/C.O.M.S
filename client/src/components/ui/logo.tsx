93d1a46e44cbd7486c124675b5a495ad55b24405
Xaidel <abechuelak@gmail.com>
2024-10-20 22:54:27 +0800

    fix:separator in login page

--- client/src/components/ui/logo.tsx
+++ client/src/components/ui/logo.tsx
@@ -1,14 +1,16 @@
 import logo from "../../assets/unc_logo.svg";
+import { Separator } from "./separator";
 
 function Logo() {
   return (
     <>
       <div className="flex flex-col items-center justify-center md:h-full w-full">
         <div className="flex items-center justify-center gap-1.5">
-          <div className="h-[15rem] w-[15rem] flex flex-row border-r-2 border-gray/60 pr-4 mr-4">
+          <div className="h-[15rem] w-[15rem] flex flex-row border-gray/60 ">
             <img src={logo} alt="CSPROBE Logo" />
           </div>
-          <div>
+          <Separator orientation="vertical" className="m-4 w-0.5 bg-gray/50" />
+          <div className="ml-3">
             <h1 className="font-bold text-red text-7xl">C.O.M.S</h1>
             <p className="font-light text-gray text-4xl">
               Course Outcome
