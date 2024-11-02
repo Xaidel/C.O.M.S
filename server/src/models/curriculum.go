2c9c8a964e8e22ca7313357f7f81be66d594ce6e
Xaidel <abechuelak@gmail.com>
2024-11-03 02:45:16 +0800

    feat: fix db constraints

--- server/src/models/curriculum.go
+++ server/src/models/curriculum.go
@@ -1,10 +1,10 @@
 package models
 
 type Curriculum struct {
-	Effectivity_Sem string
+	Effectivity_Sem string `gorm:"not null"`
 	Effectivity_SY  string `gorm:"size:4; not null"`
-	CMO_Name        string
-	IsActive        uint `gorm:"type:tinyint;not null"`
-	Revision_No     uint `gorm:"not null"`
+	CMO_Name        string `gorm:"not null"`
+	IsActive        uint   `gorm:"type:tinyint;not null"`
+	Revision_No     uint   `gorm:"not null"`
 	ID              uint
 }
--- server/src/models/user.go
+++ server/src/models/user.go
@@ -1,10 +1,10 @@
 package models
 
 type User struct {
-	UserID     string
-	Password   string
-	Firstname  string
-	Middlename string
-	Lastname   string
+	UserID     string `gorm:"not null"`
+	Password   string `gorm:"not null"`
+	Firstname  string `gorm:"not null"`
+	Middlename string `gorm:"not null"`
+	Lastname   string `gorm:"not null"`
 	ID         uint
 }
