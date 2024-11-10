81574165d3508c507df112c77a8b59cec02d9239
Xaidel <abechuelak@gmail.com>
2024-11-11 05:26:30 +0800

    fix:m2m relationship with curriculum and program

--- server/src/models/curriculum.go
+++ server/src/models/curriculum.go
@@ -1,12 +1,12 @@
 package models
 
 type Curriculum struct {
-	CMO_Name        string `gorm:"not null"`
-	Effectivity_SY  string `gorm:"size:4; not null"`
-	CurrID          string `gorm:"primaryKey"`
-	Program         Program
-	Effectivity_Sem uint `gorm:"not null"`
-	IsActive        uint `gorm:"type:tinyint;not null"`
-	Revision_No     uint `gorm:"not null"`
-	ProgramID       uint `gorm:"not null;constraints:OnUpdate:CASCADE,OnDelete:CASCADE"`
+	CMO_Name        string     `gorm:"not null"`
+	Effectivity_SY  string     `gorm:"size:4; not null"`
+	CurrID          string     `gorm:"primaryKey"`
+	Programs        []*Program `gorm:"many2many:curriculum_programs"`
+	Effectivity_Sem uint       `gorm:"not null"`
+	IsActive        uint       `gorm:"type:tinyint;not null"`
+	Revision_No     uint       `gorm:"not null"`
+	ProgramID       uint       `gorm:"not null;constraints:OnUpdate:CASCADE,OnDelete:CASCADE"`
 }
--- server/src/models/program.go
+++ server/src/models/program.go
@@ -2,9 +2,10 @@ package models
 
 type Program struct {
 	Students     []Student
-	Program_Code string     `gorm:"size:10; not null"`
-	Program_Name string     `gorm:"size:100; not null"`
-	Department   Department `gorm:"foreignKey:DepartmentID"`
-	DepartmentID uint       `gorm:"not null"`
+	Curriculums  []*Curriculum `gorm:"many2many:curriculum_programs"`
+	Program_Code string        `gorm:"size:10; not null"`
+	Program_Name string        `gorm:"size:100; not null"`
+	Department   Department    `gorm:"foreignKey:DepartmentID"`
+	DepartmentID uint          `gorm:"not null"`
 	ID           uint
 }
