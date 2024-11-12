0096d3dc8b14096b73f94bfd62d61d278f538303
Xaidel <abechuelak@gmail.com>
2024-11-12 17:33:43 +0800

    fix:login response

--- server/src/models/user.go
+++ server/src/models/user.go
@@ -2,7 +2,7 @@ package models
 
 type User struct {
 	UserID     string `gorm:"primaryKey;unique;autoIncrement:false;not null"`
-	Password   string `gorm:"not null"`
+	Password   string `gorm:"not null" json:"-"`
 	Firstname  string `gorm:"not null"`
 	Middlename string `gorm:"not null"`
 	Lastname   string `gorm:"not null"`
--- server/src/services/assertrole.go
+++ server/src/services/assertrole.go
@@ -1,6 +1,9 @@
 package services
 
-import "github.com/Xaidel/server/src/models"
+import (
+	"github.com/Xaidel/server/lib"
+	"github.com/Xaidel/server/src/models"
+)
 
 func AssertRoleStruct(role string) (interface{}, error) {
 	switch role {
@@ -14,6 +17,33 @@ func AssertRoleStruct(role string) (interface{}, error) {
 		return &models.Faculty{}, nil
 	case "Student":
 		return &models.Student{}, nil
+	default:
+		return nil, nil
+	}
+}
+
+func PreloadLoginInfo(role string, userID string) (interface{}, error) {
+	switch role {
+	case "Dean":
+		var model models.Dean
+		err := lib.Database.Preload("User").Preload("Department").First(&model, "user_id = ?", userID).Error
+		return model, err
+	case "Assistant Dean":
+		var model models.AssistantDean
+		err := lib.Database.Preload("User").Preload("Department").First(&model, "user_id = ?", userID).Error
+		return model, err
+	case "Program Head":
+		var model models.ProgramHead
+		err := lib.Database.Preload("User").Preload("Program.Department").First(&model, "user_id = ?", userID).Error
+		return model, err
+	case "Faculty":
+		var model models.Faculty
+		err := lib.Database.Preload("User").Preload("Department").First(&model, "user_id = ?", userID).Error
+		return model, err
+	case "Student":
+		var model models.Student
+		err := lib.Database.Preload("User").Preload("Program.Department").First(&model, "user_id = ?", userID).Error
+		return model, err
 	default:
 		return nil, nil
 	}
