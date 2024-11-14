d8206566a8b6e77ce905c8e581a83db5c048344c
Xaidel <abechuelak@gmail.com>
2024-11-14 16:25:54 +0800

    feat:program head column & fix dark effect when sidebar opens

--- client/src/components/ui/table.tsx
+++ client/src/components/ui/table.tsx
@@ -9,7 +9,7 @@ const Table = React.forwardRef<
   <div className="relative w-full overflow-auto">
     <table
       ref={ref}
-      className={cn("w-full caption-bottom text-sm", className)}
+      className={cn("w-full caption-bottom text-md", className)}
       {...props}
     />
   </div>
@@ -20,7 +20,11 @@ const TableHeader = React.forwardRef<
   HTMLTableSectionElement,
   React.HTMLAttributes<HTMLTableSectionElement>
 >(({ className, ...props }, ref) => (
-  <thead ref={ref} className={cn("[&_tr]:border-b", className)} {...props} />
+  <thead
+    ref={ref}
+    className={cn("[&_tr]:border-b text-lg", className)}
+    {...props}
+  />
 ));
 TableHeader.displayName = "TableHeader";
 
