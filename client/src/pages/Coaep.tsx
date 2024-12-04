f5071bdf5af2925218400e95abc82e3dace2e1b4
Xaidel <abechuelak@gmail.com>
2024-12-05 05:41:39 +0800

    fix:z-index issue on the coaep tab

--- client/src/pages/Coaep.tsx
+++ client/src/pages/Coaep.tsx
@@ -1,13 +1,17 @@
 import AppLabel from "@/components/ui/applabel";
+import { useSidebar } from "@/components/ui/sidebar";
 import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import CoaepTabContent from "@/features/faculty/CoaepTabContent";
 
 export default function Coaep() {
+  const { state } = useSidebar();
+  console.log(state);
   return (
     <>
       <div>
         <AppLabel currentPage="Course Outcome Assessment and Evaluation Plan" />
       </div>
-      <Tabs defaultValue="co" className="w-full flex justify-center">
+      <Tabs defaultValue="co" className="w-full flex flex-col justify-center">
         <TabsList className="h-auto p-0 bg-transparent gap-8">
           {[
             { value: "co", label: "Course Outcome" },
@@ -19,14 +23,15 @@ export default function Coaep() {
             <TabsTrigger
               key={tab.value}
               value={tab.value}
-              className="relative h-auto px-0 pb-4 pt-10 data-[state=active]:bg-transparent font-normal text-muted-foreground data-[state=active]:text-foreground data-[state=active]:shadow-none 
-                after:absolute after:z-20 after:border-2 after:left-1/2 after:top-0 after:h-[25px] after:w-[25px] after:-translate-x-1/2 after:rounded-full after:bg-[#F8F8F8] after:border-[#CBD2DB] data-[state=active]:after:border-4 data-[state=active]:after:border-[#40454E]
-                before:absolute before:z-10 before:top-[0.35rem] before:left-[-50%] before:right-[-50%] before:h-[13px] before:min-w-full before:rounded-md before:bg-[#CBD2DB]"
+              className={`relative h-auto px-0 pb-4 pt-10 data-[state=active]:bg-transparent font-normal text-muted-foreground data-[state=active]:text-foreground data-[state=active]:shadow-none 
+                after:absolute after:z-20 ${state === "expanded" && "after:z-1"} after:border-2 after:left-1/2 after:top-0 after:h-[25px] after:w-[25px] after:-translate-x-1/2 after:rounded-full after:bg-[#F8F8F8] after:border-[#CBD2DB] data-[state=active]:after:border-4 data-[state=active]:after:border-[#40454E]
+                before:absolute before:z-10 ${state === "expanded" && "before:z-0"} before:top-[0.35rem] before:left-[-40%] before:right-[-40%] before:h-[13px] before:min-w-full before:rounded-md before:bg-[#CBD2DB]`}
             >
               {tab.label}
             </TabsTrigger>
           ))}
         </TabsList>
+        <CoaepTabContent />
       </Tabs>
     </>
   );
