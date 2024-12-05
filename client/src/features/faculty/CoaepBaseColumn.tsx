da997b4ce1f4fe366abefd3804d145e233192397
Xaidel <abechuelak@gmail.com>
2024-12-06 03:05:37 +0800

    feat:coaep base tables

---
+++ client/src/features/faculty/CoaepBaseColumn.tsx
@@ -0,0 +1,10 @@
+import { CourseOutcome } from "@/types/Interface";
+import { ColumnDef } from "@tanstack/react-table";
+
+export const CoaepBaseColumn: ColumnDef<CourseOutcome>[] = [
+  {
+    header: "CO#",
+    accessorKey: "ID",
+  },
+  { header: "Statement", accessorKey: "Statement" },
+];
