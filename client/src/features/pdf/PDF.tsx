c5012111550d08063e90143824da7b4c362204ad
Xaidel <abechuelak@gmail.com>
2025-04-05 20:35:31 +0800

    feat: table header

--- client/src/features/pdf/PDF.tsx
+++ client/src/features/pdf/PDF.tsx
@@ -1,13 +1,40 @@
 import { Image, Document, Page, Text, View, PDFViewer } from '@react-pdf/renderer';
-import { styles } from "./Stylesheet.tsx"
+import { styles, tables } from "./Stylesheet.tsx"
 import logo from "../../assets/unc_logo.png"
 import { useUser } from '../auth/useUser.ts';
+import { useCurrentPeriod } from '../auth/useCurrentPeriod.ts';
+import { Period } from '@/types/Interface.ts';
 
+const formatYear = (current: Period) => {
+  return ` 20${current.School_Year.slice(0, 2)}-20${current.School_Year.slice(2)}`;
+};
+
+const formatSem = (current: Period) => {
+  let formattedSem;
+  if (current.Semester === 1) {
+    formattedSem = `${current?.Semester}st Semester`;
+  } else if (current.Semester === 2) {
+    formattedSem = `${current?.Semester}nd Semester`;
+  } else {
+    formattedSem = `Summer Semester`;
+  }
+  return formattedSem
+}
 export const PDF = () => {
   const { currentUser } = useUser()
   const { User } = currentUser?.role_info
   const middleInitial = User?.Middlename !== undefined ? `${User?.Middlename.charAt(0)}.` : ""
   const fullname = `${User?.Firstname} ${middleInitial} ${User.Lastname}`
+  const selected = sessionStorage.getItem("selectedCourse")
+  const parsedSelected = selected ? JSON.parse(selected) : null
+  const { Course } = parsedSelected
+  const { response } = useCurrentPeriod()
+  let currentYear;
+  let currentSem
+  if (response?.current_period) {
+    currentYear = formatYear(response?.current_period)
+    currentSem = formatSem(response?.current_period)
+  }
   return (
     <div className='w-full h-full'>
       <PDFViewer
@@ -42,24 +69,52 @@ export const PDF = () => {
                 </View>
                 <View style={{ flexDirection: "row", alignItems: "center" }}>
                   <Text>Course: </Text>
-                  <Text style={{ fontWeight: "bold" }}>{`BIT212k (Object-Oriented Programming 1)`}</Text>
+                  <Text style={{ fontWeight: "bold" }}>{`${Course.Course_No} (${Course.Course_Name})`}</Text>
 
                 </View>
               </View>
               <View style={{ margin: "0 30px" }}>
                 <View style={{ flexDirection: "row", alignItems: "center" }}>
                   <Text>School Year: </Text>
-                  <Text style={{ fontWeight: "bold", }}>{`2022-2023`}</Text>
+                  <Text style={{ fontWeight: "bold", }}>{currentYear}</Text>
                 </View>
                 <View style={{ flexDirection: "row", alignItems: "center" }}>
                   <Text>Semester: </Text>
-                  <Text style={{ fontWeight: "bold" }}>{`1st Semester`}</Text>
+                  <Text style={{ fontWeight: "bold" }}>{currentSem}</Text>
+                </View>
+              </View>
+            </View>
+            <View style={tables.table}>
+              <View style={tables.tableRow}>
+                <View style={[tables.colHeader, { width: "20%" }]}>
+                  <Text style={tables.tableCellHeader}>Course Outcome Statements</Text>
+                </View>
+                <View style={[tables.colHeader, { width: "25%" }]}>
+                  <Text style={tables.tableCellHeader}>Intended Learning Outcomes Statements</Text>
+                </View>
+                <View style={[tables.colHeader, { width: "10%" }]}>
+                  <Text style={tables.tableCellHeader}>Assessment Tool</Text>
+                </View>
+                <View style={[tables.colHeader, { width: "15%" }]}>
+                  <Text style={tables.tableCellHeader}>Performance Target</Text>
+                </View>
+                <View style={[tables.colHeader, { width: "5%" }]}>
+                  <Text style={tables.tableCellHeader}>#</Text>
+                </View>
+                <View style={[tables.colHeader, { width: "5%" }]}>
+                  <Text style={tables.tableCellHeader}>%</Text>
+                </View>
+                <View style={[tables.colHeader, { width: "10%" }]}>
+                  <Text style={tables.tableCellHeader}>Evaluation</Text>
+                </View>
+                <View style={[tables.colHeader, { width: "15%" }]}>
+                  <Text style={tables.tableCellHeader}>Recommendation</Text>
                 </View>
               </View>
             </View>
           </Page>
         </Document>
       </PDFViewer>
-    </div>
+    </div >
   )
 }
--- client/src/features/pdf/Stylesheet.tsx
+++ client/src/features/pdf/Stylesheet.tsx
@@ -2,7 +2,8 @@ import { StyleSheet } from "@react-pdf/renderer"
 export const styles = StyleSheet.create({
   container: {
     flexDirection: "row",
-    width: "100%"
+    width: "100%",
+    padding: "20px",
   },
   text: {
     flexDirection: "column",
@@ -29,6 +30,41 @@ export const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-between",
     fontSize: "0.6rem",
-    padding: "15px 0",
+    paddingLeft: "25px",
+    paddingRight: "25px",
+    marginBottom: "5px",
   }
 });
+
+export const tables = StyleSheet.create({
+  table: {
+    marginLeft: 20,
+    marginRight: 20,
+    display: "flex",
+    width: "auto",
+    borderStyle: "solid",
+    borderWidth: 1,
+    borderColor: "#000",
+    borderRightWidth: 0,
+    borderBottomWidth: 0,
+  },
+  tableRow: {
+    flexDirection: "row",
+    borderBottomWidth: 1,
+    borderBottomColor: "#000",
+  },
+  colHeader: {
+    textAlign: "center",
+    fontWeight: "bold",
+    borderStyle: "solid",
+    borderRightWidth: 1,
+    borderColor: "#000",
+    padding: 5,
+    backgroundColor: "#E6E9EF",
+  },
+  tableCellHeader: {
+    fontWeight: "bold",
+    fontSize: 8,
+  },
+})
+
