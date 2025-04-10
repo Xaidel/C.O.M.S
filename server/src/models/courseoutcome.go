b36738bd64b71fb03b01151f9726d024b823fcc3
Xaidel <abechuelak@gmail.com>
2025-04-11 00:26:19 +0800

    feat: add level column in course outcome

--- server/src/models/courseoutcome.go
+++ server/src/models/courseoutcome.go
@@ -4,6 +4,7 @@ type CourseOutcome struct {
 	IntendedLearningOutcomes []*IntendedLearningOutcome
 	CoeapID                  *uint
 	Statement                string `gorm:"size:300; not null"`
+	Level                    string `gorm:"size:50; not null"`
 	Coeap                    Coeap  `gorm:"foreignKey:CoeapID;references:ID;"`
 	ID                       uint
 }
